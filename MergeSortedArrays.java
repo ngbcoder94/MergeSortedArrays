b0b6af66548b9f133ad3dad81e0f9a0691f0a72f
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-02 22:44:06 -0500

    MergeSortedArrays class created for solution

---
+++ MergeSortedArrays.java
@@ -0,0 +1,15 @@
+/*
+ * This is my clas for the Merging of two sorted arrays problem from Leetcode.com. 
+ *
+ * @Author - Nick Burkett. 
+ * @Date - 01/02/2025.
+ *
+ * Satus - WIP.
+ */
+public class MergeSortedArrays{
+
+        public static void main(String[] args){
+                
+                System.out.println("Hello World");
+        }
+}
