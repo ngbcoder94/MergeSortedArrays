00ca52caaf500bcc1945c0fc6edbf1b1792464a8
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-05 17:54:19 -0500

    Making progress on solution()

--- MergeSortedArrays.java
+++ MergeSortedArrays.java
@@ -24,6 +24,19 @@ public class MergeSortedArrays{
                         //Then I need to loop through nums1 for each iteration of i - aka bubble sort.
                         for(int j = 0; j < L1; j++){
                                 
+                                //Here I check if the current element of nums2 <= current element of nums1.
+                                if(nums2[i] <= nums1[j]){
+                                        insert(nums1, j, nums2[i]);     //Insert the new value into nums1.
+                                        L1 +=1;                         //Increment L1 by one since we added a value.
+                                }
+                                //Now I need to check if current val of nums2 > current val of nums1. 
+                                else if((nums2[i] > nums1[j]) && (j == (L1-1))){
+                                        nums1[j+1] = nums2[i];          //Add the value in the next place holder. 
+                                        L1 += 1;                        //Increment the length of nums1 count after adding.
+                                }
+                                else if(){
+
+                                }
 
                         }
                 }
