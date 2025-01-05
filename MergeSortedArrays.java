613633c4a550ccaa54f5a3a584e7188db4adc512
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-05 17:34:07 -0500

    solution function created

--- MergeSortedArrays.java
+++ MergeSortedArrays.java
@@ -7,7 +7,19 @@
  * Satus - WIP.
  */
 public class MergeSortedArrays{
-
+        
+        /*
+         * Here is my function that will Sort and merge the two arrays.
+         *
+         * @param int[] nums1, nums2 - Two arrays that contain the arrays I am working with.
+         * @param int L1, L2 - The length of of the two arrays above.
+         *
+         * @return void - Don't return anything
+         */
+        public void solution(int[] nums1, int[] nums2, int L1, int L2){
+                
+                
+        }
         public static void main(String[] args){
                 
                 System.out.println("Hello World");
