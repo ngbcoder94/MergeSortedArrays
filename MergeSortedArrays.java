3e81deaa11e84ff30351aa46398201c1a99aab64
ngbcoder94 <ngbcoder94@gmail.com>
2025-01-06 18:26:15 -0500

    Testing is looking good... more to come

--- MergeSortedArrays.java
+++ MergeSortedArrays.java
@@ -17,9 +17,10 @@ public class MergeSortedArrays{
          * int L1 - Current length of L1.
          */
         public void insert(int[] nums1, int index, int val, int L1){
-                
+               
+                //System.out.println("fuck");
                 //Need to loop through nums1 and move everything over by one.
-                for(int i = (L1-1); i > index; i--){
+                for(int i = (L1-1); i >= index; i--){
                         nums1[i+1] = nums1[i];           //Move each element to the left by one.
                 }
 
@@ -44,14 +45,17 @@ public class MergeSortedArrays{
                                 
                                 //Here I check if the current element of nums2 <= current element of nums1.
                                 if(nums2[i] <= nums1[j]){
-                                        insert(nums1, j, nums2[i], L1);     //Insert the new value into nums1.
+                                        //System.out.println("idk");
+                                        insert(nums1, j, nums2[i], L1);     //Insert the new value into nums1.  
                                         L1 +=1;                         //Increment L1 by one since we added a value.
+                                        break;
                                 }
 
                                 //Now I need to check if current val of nums2 > current val of nums1. 
                                 else if((nums2[i] > nums1[j]) && (j == (L1-1))){
                                         nums1[j+1] = nums2[i];          //Add the value in the next place holder. 
                                         L1 += 1;                        //Increment the length of nums1 count after adding.
+                                        break;
                                 }
 
                                 //Need to check if I am at my last element in each array and nums2[i] > nums1[j]. 
@@ -64,6 +68,16 @@ public class MergeSortedArrays{
         }
         public static void main(String[] args){
                 
-                System.out.println("Hello World");
+                int[] testArr1 = {1, 3, 5, 0, 0, 0};                        //Create nums1 for testing.
+                int[] testArr2 = {0, 2, 4};                           //Create nums2 for testing.
+
+                MergeSortedArrays tmpObj = new MergeSortedArrays();         //Create instance of MergeSortedArrays for testing.
+
+                tmpObj.solution(testArr1, testArr2, 3, 3);                  //Call solution and run test.
+                
+                //Loop through testArr1 to see if these were merged and sorted correctly.
+                for(int i = 0; i < testArr1.length; i ++){
+                        System.out.println(testArr1[i]);
+                } 
         }
 }
